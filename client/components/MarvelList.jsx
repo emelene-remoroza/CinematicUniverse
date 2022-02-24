b770a6b1b84e6138abd4c2900776dfa74bad34ad
chrisjrh <chrisjrhunt@gmail.com>
2022-02-25 10:55:19 +1300

    ordered lists showing poster and link to page working

--- client/components/MarvelList.jsx
+++ client/components/MarvelList.jsx
@@ -1,22 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 import { fetchMarvel } from '../actions/marvel'
 
 function MarvelList () {
   const list = useSelector(state => state.marvel)
   const dispatch = useDispatch()
+  const [order, setOrder] = useState('ChronoDate')
   useEffect(() => {
     dispatch(fetchMarvel())
   }, [])
+  const sortedList = list.sort((a, b) => new Date(a[order]) - new Date(b[order]))
 
   return (
 
     <div>
       <h1>Marvel Movie List </h1>
+      <button onClick={() => { setOrder('ChronoDate') }}>Sort by Chronological Order</button>
+      <button onClick={() => { setOrder('Released') }}>Sort by Release Date</button>
       <ul>
-        {list.map(movie => (
-          <li key={movie.id}>{JSON.stringify(movie)}</li>
+        {sortedList.map(movie => (
+          <Link to={`${movie.id}`} key={movie.id}>
+            <li>
+              <h3>{movie.Title}</h3>
+              <p>{new Date(movie[order]).getFullYear()}</p>
+              <img height={250} src={`/images/marvel/${movie.Image}`} alt={`Image of ${movie.Title}`} /><br/>
+            </li>
+          </Link>
         ))}
       </ul>
     </div>
