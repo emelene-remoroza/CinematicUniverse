bfcbaaebcfde6caa90b4f5cb72517e161c7c7c1e
krissy-legeyt <krissylegeyt@gmail.com>
2022-02-25 12:58:37 +1300

    calling external API

--- client/components/Movie.jsx
+++ client/components/Movie.jsx
@@ -1,28 +1,36 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { useParams } from 'react-router'
-import { fetchMovie } from '../actions/movie'
+import { fetchMovie } from '../apis/movie'
 
 export default function Movie (props) {
-  const dispatch = useDispatch()
   const { id } = useParams()
   const movie = useSelector(state => state[props.category].find(movie => movie.id === Number(id)))
+  const [movieDetail, setMovieDetail] = useState('')
 
   const title = movie?.Title
 
   const releaseDate = new Date(movie?.Released)
   const year = releaseDate.getFullYear()
-  console.log(title, year)
 
   useEffect(() => {
-    // fetchMovie(title, year)
-    //   .then
-  }, [])
+    fetchMovie(title, year)
+      .then(res => {
+        setMovieDetail(res)
+        return null
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
+  }, [title, year])
 
+  console.log(movieDetail)
   return (
-    <>
-      <div>{JSON.stringify(movie)}</div>
+    <div className='movie-page'>
+
       {movie && <img height={500} src={`/images/marvel/${movie?.Image}`}/>}
-      {movie && <p>{title}</p>}
-    </>)
+      {movie && <h2>{title}</h2>}
+      <h3>{movieDetail?.Year}</h3>
+      <p>{movieDetail?.Plot}</p>
+    </div>)
 }
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
 
 import marvel from './marvel'
-import movie from './movie'
 import errorMessage from './errorMessage'
 
 export default combineReducers({
   marvel,
-  movie,
   errorMessage
 })
