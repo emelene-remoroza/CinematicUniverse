4135e75b6f75b04f72d8153abd2280cba12e7570
krissy-legeyt <krissylegeyt@gmail.com>
2022-02-26 20:37:06 +1300

    error in seeds and API not being called correctly

--- server/db/seeds/Marvel.js
+++ server/db/seeds/Marvel.js
@@ -19,7 +19,7 @@ exports.seed = function (knex) {
         { id: 15, Title: 'Captain America: Civil War', Released: '2016-05-06', ChronoDate: '2016-01-25', Image: 'captain-america-civil-war.jpg', Trailer: 'https://www.youtube-nocookie.com/embed/dKrVegVI0Us' },
         { id: 16, Title: 'Black Widow', Released: '2021-07-09', ChronoDate: '2016-6-20', Image: 'black-widow-1.jpg', Trailer: 'https://www.youtube-nocookie.com/embed/ybji16u608U' },
         { id: 17, Title: 'Spider-Man: Homecoming', Released: '2017-07-07', ChronoDate: '2016-01-01', Image: 'spiderman-homecoming.jpg', Trailer: 'https://www.youtube-nocookie.com/embed/39udgGPyYMg' },
-        { id: 18, Title: 'Black Panther', Released: '2017-02-16', ChronoDate: '2016-01-01', Image: 'black-panther-1.jpg', Trailer: 'https://www.youtube-nocookie.com/embed/xjDjIWPwcPU' },
+        { id: 18, Title: 'Black Panther', Released: '2018-02-16', ChronoDate: '2016-01-01', Image: 'black-panther-1.jpg', Trailer: 'https://www.youtube-nocookie.com/embed/xjDjIWPwcPU' },
         { id: 19, Title: 'Doctor Strange', Released: '2016-11-04', ChronoDate: '2016-01-01', Image: 'doctor-strange-1.jpg', Trailer: 'https://www.youtube-nocookie.com/embed/aWzlQ2N6qqg' },
         { id: 20, Title: 'Thor: Ragnarok', Released: '2017-11-03', ChronoDate: '2017-01-01', Image: 'thor-ragnarok.jpg', Trailer: 'https://www.youtube-nocookie.com/embed/ue80QwXMRHg' },
         { id: 21, Title: 'Ant-Man and the Wasp', Released: '2018-07-06', ChronoDate: '2018-01-01', Image: 'ant-man-wasp.jpg', Trailer: 'https://www.youtube-nocookie.com/embed/8_rTIAOohas' },
