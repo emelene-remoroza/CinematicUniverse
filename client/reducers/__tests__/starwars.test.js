147578927d88d12fea22f6bf1f590c9a36bcde28
emelene-remoroza <eneleme@yahoo.com>
2022-03-02 07:54:48 +1300

    starwars test reducer done

---
+++ client/reducers/__tests__/starwars.test.js
@@ -0,0 +1,31 @@
+import starwars from '../starwars'
+
+import { setStarWars } from '../../actions/starwars'
+
+describe('checks the reducer', () => {
+  it('sets an array with an empty object as initial state', () => {
+    const state = starwars(undefined, { type: 'INIT' })
+    expect(state).toEqual([])
+  })
+
+  it('replaces state with SET_STARWARS', () => {
+    const state = starwars([], setStarWars([{ 
+      id: 1, 
+      Title: 'Star Wars: Episode I - The Phantom Menace', 
+      Released: '1999-05-19', 
+      ChronoDate: '3001-01-01', 
+      Period: '32 BBY', 
+      Image: 'starwars-ep1.jpg', 
+      Trailer: 'https://www.youtube-nocookie.com/embed/bD7bpG-zDJQ'
+    }]))
+    expect(state).toEqual([{ 
+      id: 1, 
+      Title: 'Star Wars: Episode I - The Phantom Menace', 
+      Released: '1999-05-19', 
+      ChronoDate: '3001-01-01', 
+      Period: '32 BBY', 
+      Image: 'starwars-ep1.jpg', 
+      Trailer: 'https://www.youtube-nocookie.com/embed/bD7bpG-zDJQ'
+     }])
+  })
+})
